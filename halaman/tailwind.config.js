2d9b540e574c1da1a59cc3cd3c4bcf8b3771551c
Veran Goraldy <99161813+Veran-35@users.noreply.github.com>
2025-09-03 00:36:18 +0800

    update-1

---
+++ halaman/tailwind.config.js
@@ -0,0 +1,9 @@
+        module.exports = {
+          content: [
+            "./src/**/*.{html,js,jsx,ts,vue}", // Sesuaikan dengan struktur proyek Anda
+          ],
+          theme: {
+            extend: {},
+          },
+          plugins: [],
+        }
